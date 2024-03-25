ed2078463cac9927c0a5c8e450d17b66f942e1a5
thedashpuntsag <thedashpuntsag@gmail.com>
2024-03-25 15:54:45 +0800

    finished vite config

--- frontendmasters/course-project/src/App.jsx
+++ frontendmasters/course-project/src/App.jsx
@@ -1,7 +1,26 @@
 import React from 'react';
+import { ReactDOM } from 'react-dom';
 
-const App = () => {
-  return <div>App</div>;
+const Pet = (props) => {
+  const { name, type, breed } = props;
+
+  return React.createElement('div', {}, [
+    React.createElement('h1', {}, name),
+    React.createElement('h2', {}, type),
+    React.createElement('h2', {}, breed),
+  ]);
 };
 
-export default App;
+const App = () => {
+  // createElement(elementType, attributeObject, children )
+  return React.createElement('div', { id: 'div-id', className: '' }, [
+    React.createElement('h1', {}, 'Adopt me'),
+    React.createElement(Pet, { name: 'Luna', type: 'Dog', breed: 'Havanese' }),
+    React.createElement(Pet, { name: 'Poddy', type: 'Dog', breed: 'Havanese' }),
+    React.createElement(Pet, { name: 'Quincy', type: 'Dog', breed: 'Havanese' }),
+  ]);
+};
+
+const container = document.getElementById('root');
+const root = ReactDOM.createRoot(container);
+root.render(React.createElement(App));
