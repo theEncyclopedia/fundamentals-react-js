ce3ad9f3ca7b925c158c420eddabfd1169ac5883
thedashpuntsag <thedashpuntsag@gmail.com>
2024-04-01 07:56:49 +0800

    added Error boundaries

--- frontendmasters/course-project/src/App.jsx
+++ frontendmasters/course-project/src/App.jsx
@@ -3,7 +3,7 @@ import { StrictMode } from 'react';
 import SearchParams from './SearchParams';
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import Details from './Details';
+import DetailsErrorBoundary from './Details';
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -24,7 +24,7 @@ const App = () => {
           </header>
           <Routes>
             <Route path="/" element={<SearchParams />} />
-            <Route path="/details/:id" element={<Details />} />
+            <Route path="/details/:id" element={<DetailsErrorBoundary />} />
           </Routes>
         </BrowserRouter>
       </QueryClientProvider>
