ec6313e84f03c4bdc84b1971aace13f71e80072d
thedashpuntsag <thedashpuntsag@gmail.com>
2024-04-04 07:47:17 +0800

    configured tailwindcss

--- preview/src/App.jsx
+++ preview/src/App.jsx
@@ -1,9 +1,12 @@
 import { UseRef } from './pages/hooks';
+import { Link } from 'react-router-dom';
 
 function App() {
   return (
-    <main className="w-screen h-screen p-6  | bg-red-50">
-      <UseRef />
+    <main className="w-screen h-screen p-6 | bg-red-50">
+      <div>
+        <Link to="/home">Hello</Link>
+      </div>
     </main>
   );
 }
