c6f6768a4e37deaf119722fdcc1098eed6ad7d41
thedashpuntsag <thedashpuntsag@gmail.com>
2024-12-08 10:27:52 +0800

    Boombayawh

--- frontendmasters/01.introduction-to-react-v8/src/useBreedList.js
+++
@@ -1,7 +0,0 @@
-import { useQuery } from '@tanstack/react-query';
-import fetchBreedList from './fetchBreedList';
-
-export default function useBreedList(animal) {
-  const result = useQuery({ queryKey: ['breeds', animal], queryFn: fetchBreedList });
-  return [result?.data?.breeds ?? [], result.status];
-}
